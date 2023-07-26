b67910f51a4ca8aae41f02856a7f58f8167632e6
Vilakati <nontobekovilakati@gmail.com>
2023-07-25 19:24:08 -0700

    98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - pointer to an int as parameter and updates the value it points to to 98
+ * reset_to_98 - function pointer to an int as parameter 
+ * updates the value it points to to 98
  * @n: input
  * return: n
  */
