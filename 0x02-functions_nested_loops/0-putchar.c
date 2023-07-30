13a2a44ac22d418b7899249114f3622c7f2bba3f
Vilakati <nontobekovilakati@gmail.com>
2023-07-29 18:45:13 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+*main - function prints _putchar
+*
+*Return: 0
+*/
+int main(void)
+{
+	char school[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
+}
